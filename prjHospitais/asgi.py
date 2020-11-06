059de49a1f1bd27f114aa765ee6d19d7904eb263
Vinícius Perazza <56921696+Perazza@users.noreply.github.com>
2020-11-06 10:02:25 -0300

    Alterado nome do projeto para prjHospitais

--- prjHospitais/asgi.py
+++ prjHospitais/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djangovuejs project.
+ASGI config for prjHospitais project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangovuejs.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prjHospitais.settings')
 
 application = get_asgi_application()
